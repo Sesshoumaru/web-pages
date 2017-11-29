ea30dda3c2f9c28657c280721c7a3e1264a230b6
Sesshoumaru <laobi.net@live.com>
2017-11-29 23:40:38 +0800

    weijinsup special

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -1,17 +1,24 @@
 "use strict";
 
-$(function(){
+$(function () {
     function resize() {
         var width = $(window).width();
         var isSmallScreen = width < 768;
 
         $("#main_ad > .carousel-inner > .item").each(function () {
-           var $item = $(this);
-           var imageSrc = $item.data(isSmallScreen?'image-ls':'image-lg');
-            $item.css('backgroundImage','url(' + imageSrc+')')
+            var $item = $(this);
+            var imageSrc = $item.data(isSmallScreen ? 'image-ls' : 'image-lg');
+
+            if(isSmallScreen){
+                $item.css('backgroundImage', 'url()');
+                $item.html('<img src="'+ imageSrc+'"/>');
+            }else{
+                $item.css('backgroundImage', 'url(' + imageSrc + ')');
+                $item.empty();
+            }
         });
     }
 
-    $(window).on('resize',resize).trigger('resize');
+    $(window).on('resize', resize).trigger('resize');
 });
 
